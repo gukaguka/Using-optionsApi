16f92c78500be4a2eb417b51d2a09a1e9e6fd7ce
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-12-11 13:05:29 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 >OUTPUT COURSE GOAL:<h3 :class = 'toggleGoal'> {{ courseGoal }}</h3></h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click = 'onToggleGoal'>Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+courseGoal: 'learn vue',
+toggleGoal: 'show'
+
+};
+  }, 
+  methods: {
+        onToggleGoal(){
+          this.toggleGoal === 'show' ? this.toggleGoal = 'hide' : this.toggleGoal = 'show';
+        }
+  }
+
+  }
+  
+
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+.hide{
+  visibility: hidden;
+}
+.show {
+  visibility: visible;
+}
+</style>
